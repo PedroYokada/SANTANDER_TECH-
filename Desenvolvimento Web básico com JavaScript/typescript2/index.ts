cb26a5e7d463f75aa03f909ee38254353ad91f50
PedroYokada <pedro_hlyokada@outlook.com>
2024-12-31 00:17:03 -0300

    Segunda aula TypeScript - Santander Tech +

---
+++ Desenvolvimento Web básico com JavaScript/typescript2/index.ts
@@ -0,0 +1,13 @@
+let numero = 20; // inferencia de tipo
+
+const pi = 3.1415;
+
+let nome = 'Pedro';
+
+let nome1: string = 'Pedro Henrique';
+
+let correta: boolean;
+
+let resultado = numero * pi;
+const nomeDoUsuario = prompt("Qual o seu nome ?")
+console.log(nomeDoUsuario)
