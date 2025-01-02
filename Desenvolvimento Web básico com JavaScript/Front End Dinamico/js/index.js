77adbf0d1170bc37e4b36b638a3360264ab84258
PedroYokada <pedro_hlyokada@outlook.com>
2025-01-02 00:55:30 -0300

    Aprendendo sobre DOM - Santander Tech +

--- Desenvolvimento Web básico com JavaScript/Front End Dinamico/js/index.js
+++ Desenvolvimento Web básico com JavaScript/Front End Dinamico/js/index.js
@@ -27,3 +27,53 @@ console.log(emailInput)
 
 const jsLogoImg = document.getElementById('js-logo'); 
 console.log(jsLogoImg);
+
+
+// 5- Query Selector
+
+const imagem = document.querySelector('body > img#js-logo');
+
+console.log(imagem);
+
+const paragrafos = document.querySelectorAll("p");
+console.log(paragrafos);
+
+console.clear();
+
+// Acessando/alterando o conteudo HTML das tags
+
+const primeiroParagrafo = document.querySelector('p.paragrafo');
+
+console.log(primeiroParagrafo);
+
+console.log('Conteúdo:',primeiroParagrafo.textContent);
+console.log('innerHTML:', primeiroParagrafo.innerHTML);
+
+primeiroParagrafo.textContent = '<strong>Outra coisa<strong>';
+primeiroParagrafo.innerHTML = '<strong>Outra coisa<strong>';
+
+// 
+
+console.clear();
+
+//console.log(emailInput.value);
+
+emailInput[0].value = 'pedro@email.com'
+
+// Criando elementos na página HTML
+
+
+const listaUl = document.querySelector('ul#lista');
+const listaLis = document.querySelectorAll('ul > li')
+
+const novaTagLi = document.createElement('li'); // criando uma tag li vazia
+
+novaTagLi.textContent = "Segundo item" // adicionando texto dentro da tag li
+
+console.log(novaTagLi);
+
+//listaUl.appendChild(novaTagLi);
+
+listaUl.insertBefore(novaTagLi, listaLis[1]);
+
+listaUl.removeChild(novaTagLi)
